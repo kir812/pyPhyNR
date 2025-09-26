b081151b87199c18b18ab03b719cecf2292fa8da
Kirill Klechenov <kir812@gmail.com>
2025-09-26 16:37:00 -0400

    Preparing for first release
    A bit of cleanup

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ setup(
     name="pyPhyNR",
     version="0.1.0",
     description="Python toolkit for 5G NR physical layer simulations",
-    author="Your Name",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="kir812",
+    author_email="kir812@gmail.com",
+    url="https://github.com/kir812/pyPhyNR",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
@@ -22,4 +26,13 @@ setup(
         ],
     },
     python_requires=">=3.9",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Telecommunications",
+    ],
+    keywords="5G NR, physical layer, telecommunications, signal processing",
 ) 
--- src/pyPhyNR/__init__.py
+++ src/pyPhyNR/__init__.py
@@ -7,6 +7,10 @@ __version__ = "0.1.0"
 from . import core
 from . import waveforms
 from . import utils
+from .core.signal_builder import NRSignalBuilder
 
 # Recommended usage in documentation
-__recommended_import__ = "import pyPhyNR as pynr" 
+__recommended_import__ = "import pyPhyNR as pynr"
+
+# Make commonly used classes available in the root namespace
+__all__ = ['NRSignalBuilder'] 
